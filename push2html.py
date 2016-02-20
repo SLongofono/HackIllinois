2b5e66ce2cd8d0185a66ebf791ea3ce9582a50cc
Stephen Longofono <onofognol@ku.edu>
2016-02-20 16:21:28 -0600

    Implemented naming feature, see comments

--- push2html.py
+++ push2html.py
@@ -6,10 +6,22 @@ import paragraph as para
 #black magic to fetch stdout
 #output = subprocess.Popen(command, close_fds=True, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
-output = para.genCopy(os.getcwd(), os.getcwd() + "/p1.txt", 4)
+copy, name = para.genCopy(os.getcwd(), os.getcwd() + "/p1.txt")
 os.system('cp template.html index.html')
-command = 'sed -i s/qqqqq/' + '\"' + output[0] + '\"' + '/g index.html'
-os.system(command)
+print copy, name
+#qqqqq for copy
+command1 = 'sed -i s/qqqqq/' + '\"' + copy + '\"' + '/g index.html'
 
+#nnnnn for name
+command2 = 'sed -i s/nnnnn/' + '\"' + name + '\"' + '/g index.html'
+
+#bbbbb for something
+#command = 'sed -i s/nnnnn/' + '\"' + output[0] + '\"' + '/g index.html'
+
+#ccccc for something else
+#command = 'sed -i s/nnnnn/' + '\"' + output[0] + '\"' + '/g index.html'
+
+os.system(command1)
+os.system(command2)
 #print output[0]
 #print output[1]

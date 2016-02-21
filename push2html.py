1260aeb89365928e13398663dc39ab5cbb1d9cf2
Stephen Longofono <onofognol@ku.edu>
2016-02-20 21:55:56 -0600

    git says so"

--- push2html.py
+++ push2html.py
@@ -6,22 +6,27 @@ import paragraph as para
 #black magic to fetch stdout
 #output = subprocess.Popen(command, close_fds=True, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
-copy, name = para.genCopy(os.getcwd(), os.getcwd() + "/p1.txt")
-os.system('cp template.html index.html')
-print copy, name
+quote1, nil = para.genCopy(os.getcwd(), os.getcwd() + "/quote1.txt")
+quote2, nil = para.genCopy(os.getcwd(), os.getcwd() + "/quote2.txt")
+quote3, nil = para.genCopy(os.getcwd(), os.getcwd() + "/quote1.txt")
+#copy, name = para.genCopy(os.getcwd(), os.getcwd() + "/p1.txt")
+
+os.system('cp template1.html index.html')
+
 #qqqqq for copy
-command1 = 'sed -i s/qqqqq/' + '\"' + copy + '\"' + '/g index.html'
+command1 = 'sed -i s/qqqqq/' + '\"' + quote1 + '\"' + '/g index.html'
 
-#nnnnn for name
-command2 = 'sed -i s/nnnnn/' + '\"' + name + '\"' + '/g index.html'
+#rrrrr for name
+command2 = 'sed -i s/rrrrr/' + '\"' + quote2 + '\"' + '/g index.html'
 
-#bbbbb for something
-#command = 'sed -i s/nnnnn/' + '\"' + output[0] + '\"' + '/g index.html'
+#sssss for something
+command3 = 'sed -i s/sssss/' + '\"' + quote3 + '\"' + '/g index.html'
 
 #ccccc for something else
 #command = 'sed -i s/nnnnn/' + '\"' + output[0] + '\"' + '/g index.html'
 
 os.system(command1)
 os.system(command2)
+os.system(command3)
 #print output[0]
 #print output[1]

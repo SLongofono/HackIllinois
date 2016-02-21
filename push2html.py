b0ed35f8b449408fcab8e9ee393867990973cb6d
Stephen Longofono <onofognol@ku.edu>
2016-02-21 02:39:20 -0600

    tweaks

--- push2html.py
+++ push2html.py
@@ -12,16 +12,15 @@ else:
 	target = 'target2.html'
 
 #command = "python paragraph.py . ./p1.txt 4"
-#black magic to fetch stdout
 #output = subprocess.Popen(command, close_fds=True, shell=True, stdout=subprocess.PIPE).communicate()[0]
 quote1, nil = para.genCopy(os.getcwd(), os.getcwd() + "quote1.txt")
 quote2, nil = para.genCopy(os.getcwd(), os.getcwd()+"quote2.txt")
 quote3, nil = para.genCopy(os.getcwd(), os.getcwd() +"../quote1.txt")
 #copy, name = para.genCopy(os.getcwd(), os.getcwd() + "/p1.txt")
 
-#TODO change one to
+
 os.system('cp template1.html target1.html')
-os.system('cp template1.html target2.html')
+os.system('cp template2.html target2.html')
 
 #qqqqq for copy
 command1 = 'sed -i s/qqqqq/' + '\"' + quote1 + '\"' + '/g ' + target

dc313634103c3c08f7f109187f243ccfb9be457b
Stephen Longofono <onofognol@ku.edu>
2016-02-20 23:39:52 -0600

    updating

--- flaskillinois.py
+++ flaskillinois.py
@@ -4,15 +4,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/target1.html")
 def hello():
-	command = "python push2html.py"
+	command = "python push2html.py 1"
 	os.system(command)
-	x = open("index.html", "r")
+
+	x = open("target1.html", "r")
 	page = x.read()
 	x.close()
 	print page
 	return page
+
+@app.route("/target2.html")
+def hello2():
+	command = "python push2html.py 0"
+	os.system(command)
+
+	x = open("target2.html", "r")
+	page = x.read()
+	x.close()
+	print page
+	return page
+
 
 if __name__ == "__main__":
 	app.run()
--- push2html.py
+++ push2html.py
@@ -1,6 +1,15 @@
 import os
 import subprocess
 import paragraph as para
+import sys
+
+target = ''
+if sys.argv[1] == 1:
+	print "FOUND A ONE"
+	target = 'target1.html'
+else:
+	print "FOUND A TWO"
+	target = 'target2.html'
 
 #command = "python paragraph.py . ./p1.txt 4"
 #black magic to fetch stdout
@@ -11,19 +20,21 @@ quote2, nil = para.genCopy(os.getcwd(), os.getcwd() + "/quote2.txt")
 quote3, nil = para.genCopy(os.getcwd(), os.getcwd() + "/quote1.txt")
 #copy, name = para.genCopy(os.getcwd(), os.getcwd() + "/p1.txt")
 
-os.system('cp template1.html index.html')
+#TODO change one to
+os.system('cp template1.html target1.html')
+os.system('cp template1.html target2.html')
 
 #qqqqq for copy
-command1 = 'sed -i s/qqqqq/' + '\"' + quote1 + '\"' + '/g index.html'
+command1 = 'sed -i s/qqqqq/' + '\"' + quote1 + '\"' + '/g ' + target
 
 #rrrrr for name
-command2 = 'sed -i s/rrrrr/' + '\"' + quote2 + '\"' + '/g index.html'
+command2 = 'sed -i s/rrrrr/' + '\"' + quote2 + '\"' + '/g ' + target
 
 #sssss for something
-command3 = 'sed -i s/sssss/' + '\"' + quote3 + '\"' + '/g index.html'
+command3 = 'sed -i s/sssss/' + '\"' + quote3 + '\"' + '/g ' + target
 
 #ccccc for something else
-#command = 'sed -i s/nnnnn/' + '\"' + output[0] + '\"' + '/g index.html'
+#command = 'sed -i s/nnnnn/' + '\"' + output[0] + '\"' + '/g ' + target
 
 os.system(command1)
 os.system(command2)
